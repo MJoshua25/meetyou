0e1f9c13b65a91157cd016367c3bb7d4e6e12c7e
Romaric Ano <jeannoelromaricano@gmail.com>
2020-06-01 15:36:43 +0000

    ajouts fonction getCritere et getCi

--- php/functions.php
+++ php/functions.php
@@ -7,9 +7,56 @@
     if (date('md') < date('md', strtotime($date))) {
       return $age - 1;
     }
-    
+
     return $age;
   }
+
+
+  // function getMatchs($user){
+  //   $allUsers = [];
+  //   $bdd = Database::connect();
+  //   $req = $bdd -> prepare('SELECT * FROM individu WHERE id != :id_user'); // get all individu except $user
+  //   $req -> execute(array('id_user' => $user->id));
+  //   while($data = $req -> fetch()){  // if user exists
+  //     $indiv = new Individu($data);
+  //     $indiv->destroyPassword();
+  //     array_push($allUsers,$indiv);
+  //   }
+  //   $req -> closeCursor(); // end of processing
+  //   $bdd = Database::disconnect();
+  //
+  // }
+
+  function getCritere($user){
+    $criteres = [];
+    $bdd = Database::connect();
+    $req = $bdd -> prepare('SELECT age_deb, age_fin, sexe, teint, taille_deb, taille_fin, morphologie, nationalite, religion FROM critere WHERE id_critere = :id_user');
+    $req -> execute(array('id_user' => $user->id_critere));
+    $data = $req -> fetch();
+    // array_push($criteres,$user->id);
+    array_push($criteres,$data['age_deb']);
+    array_push($criteres,$data['age_fin']);
+    array_push($criteres,$data['sexe']);
+    array_push($criteres,$data['teint']);
+    array_push($criteres,$data['taille_deb']);
+    array_push($criteres,$data['taille_fin']);
+    array_push($criteres,$data['morphologie']);
+    array_push($criteres,$data['nationalite']);
+    array_push($criteres,$data['religion']);
+
+    return $criteres;
+  }
+
+  function getCi($user){
+    $bdd = Database::connect();
+    $req = $bdd -> prepare('SELECT ci FROM centre_interet WHERE id_ci = :id_user');
+    $req -> execute(array('id_user' => $user->id_ci));
+    $data = $req -> fetch();
+    $ci = explode(",",$data['ci']);
+
+    return $ci;
+  }
+
 
 
  ?>
